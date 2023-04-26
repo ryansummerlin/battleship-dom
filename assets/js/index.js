26b528a630ae578734e808dd3af1bfc3233a1761
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-26 00:52:17 -0700

    Finished the game over functionality as well as the reset button

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,19 +21,54 @@ for (let i = 0; i < 9; i++) {
     }
 }
 
+const resetButton = document.createElement("button");
+resetButton.setAttribute("type", "button");
+resetButton.setAttribute("class","reset-button");
+resetButton.innerText = "Reset Game";
+document.body.appendChild(resetButton);
+
+const clickResetButton = function(event) {
+    // You could just call location.reload which is a lot quicker but I think it defeats
+    // the whole purpose of the exercise to do that so I've taken the long way so to speak
+
+    let gridItems = gridContainer.children;
+    for (let i = 0; i < gridItems.length; i++) {
+        let box = gridItems[i];
+        box.setAttribute("class", "grid-item");
+        box.innerText = '';
+    }
+
+    if (board.isGameOver()) {
+        const winningMessage = document.querySelector("h2");
+        console.log(winningMessage);
+        winningMessage.remove();
+        gridContainer.addEventListener("click", clickOnGrid);
+    }
+
+    board = new Board();
+}
+
+const clickOnGrid = function(event) {
+    let box = event.target;
+    let row = box.dataset.row;
+    let col = box.dataset.col;
+    let val = board.makeHit(row, col);
+    if (val === null) {
+        box.classList.add("class", "miss");
+    } else {
+        box.classList.add("class", "hit");
+        box.innerText = val;
+    }
+
+    if (board.isGameOver()) {
+        const winningMessage = document.createElement("h2");
+        winningMessage.innerText = "YOU WIN!";
+        document.body.appendChild(winningMessage);
+        gridContainer.removeEventListener("click", clickOnGrid);
+    }
+}
 
 window.addEventListener("DOMContentLoaded", event => {
-    gridContainer.addEventListener("click", event => {
-        let box = event.target;
-        // [row, col] = [box.dataset.row, box.dataset.col];
-        let row = box.dataset.row;
-        let col = box.dataset.col;
-        let val = board.makeHit(row, col);
-        if (val === null) {
-            box.setAttribute("class", "miss");
-        } else {
-            box.setAttribute("class", "hit");
-            box.innerText = val;
-        }
-    });
+    gridContainer.addEventListener("click", clickOnGrid);
+    resetButton.addEventListener("click", clickResetButton);
 });
